501a7e3d2f7e500fd3521d7f9cda5c656c33e5e5
bhuwan <bhuwanchandra.it@gmail.com>
2021-05-11 07:27:50 +0530

    Added Google analytics

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
+import ReactGA from 'react-ga';
 
 function App() {
+  React.useEffect(()=>{
+    ReactGA.initialize('G-7XPBWVV3BQ');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  })
+
   return (
     <>
       <Header/>
