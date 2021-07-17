5277afa62873e63cfbdef734d88263cc04bd530e
kamal <kkjoshi.it@gmail.com>
2021-07-17 08:33:06 +0530

    add download event

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faWindows, faApple, faLinux} from '@fortawesome/free-brands-svg-icons';
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
+import ReactGA from 'react-ga';
+
 export default function Home() {
   let [tag, setTag] = useState(null);
 
   let [downloadHref, setDownloadHref] = useState("#download");
+  let [os, setOs] = useState("unknown");
   function getOSIcon() {
     if (downloadHref.endsWith(".exe")) {
       return faWindows;
@@ -33,10 +36,22 @@ export default function Home() {
       const response = await fetch('https://api.github.com/repos/upnotes-io/upnotes-website/tags');
       const tags = await response.json(); //extract JSON from the http response
       setTag(tags[0].name.substr(1)); // Ex: v1.0.3-beta but download link require without v.
-      if (navigator.appVersion.indexOf("Win") !== -1) setDownloadHref(getWindowsHref(tag));
-      if (navigator.appVersion.indexOf("Mac") !== -1) setDownloadHref(getMacHref(tag));
-      if (navigator.appVersion.indexOf("X11") !== -1) setDownloadHref(getLinuxHref(tag));
-      if (navigator.appVersion.indexOf("Linux") !== -1) setDownloadHref(getLinuxHref(tag));
+      if (navigator.appVersion.indexOf("Win") !== -1) {
+        setDownloadHref(getWindowsHref(tag));
+        setOs("windows");
+      }
+      if (navigator.appVersion.indexOf("Mac") !== -1) {
+        setDownloadHref(getMacHref(tag));
+        setOs("mac");
+      }
+      if (navigator.appVersion.indexOf("X11") !== -1) {
+        setDownloadHref(getLinuxHref(tag));
+        setOs("linux");
+      }
+      if (navigator.appVersion.indexOf("Linux") !== -1) {
+        setDownloadHref(getLinuxHref(tag));
+        setOs("linux");
+      }
     }
     getTag();
   }, [tag]);
@@ -74,10 +89,14 @@ export default function Home() {
         </div>
 
         <h2 className="w3-xlarge w3-padding-32" style={{color: 'black'}}>Automatically organize your software engineering notes.</h2>
-        <a href={downloadHref} className="w3-button w3-text-black w3-padding-large w3-large w3-margin-top">
+        <ReactGA.OutboundLink
+            className="w3-button w3-text-black w3-padding-large w3-large w3-margin-top"
+            eventLabel={`download-1-${os}`}
+            to={downloadHref}
+        >
           <FontAwesomeIcon icon={getOSIcon()} />
           <span> Download </span>
-        </a>
+        </ReactGA.OutboundLink>
       </div>
       <div className="w3-row-padding w3-padding-64 w3-container">
         <div className="w3-content">
@@ -107,9 +126,9 @@ export default function Home() {
                 'margin': 'auto',
                 'font-size': 'xx-large',
               }}>
-                <div><a href={getWindowsHref()}><FontAwesomeIcon icon={faWindows} /> <span>Windows</span></a></div>
-                <div><a href={getLinuxHref()}><FontAwesomeIcon icon={faLinux} /> <span>Linux</span></a></div>
-                <div><a href={getMacHref()}><FontAwesomeIcon icon={faApple} /> <span>Mac</span></a></div>
+                <div><ReactGA.OutboundLink eventLabel="download-2-windows" to={getWindowsHref()}><FontAwesomeIcon icon={faWindows} /> <span>Windows</span></ReactGA.OutboundLink></div>
+                <div><ReactGA.OutboundLink eventLabel="download-2-linux" to={getLinuxHref()}><FontAwesomeIcon icon={faLinux} /> <span>Linux</span></ReactGA.OutboundLink></div>
+                <div><ReactGA.OutboundLink eventLabel="download-2-mac" to={getMacHref()}><FontAwesomeIcon icon={faApple} /> <span>Mac</span></ReactGA.OutboundLink></div>
               </div>
             </div>
         }
