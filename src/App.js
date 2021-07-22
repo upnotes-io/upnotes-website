a75766b41cc1abb7fe52fd7503cd3ea9e58e2079
kamal <kkjoshi.it@gmail.com>
2021-07-22 07:59:54 +0530

    fix env check

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
 import ReactGA from 'react-ga';
-import process from "process";
-const development = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 function App() {
 
   React.useEffect(()=>{
     console.log("env:" + process.env.NODE_ENV);
-    if (!development) {
+    if (process.env.NODE_ENV !== 'development') {
       ReactGA.initialize('UA-196765342-1', {debug: true});
       ReactGA.pageview(window.location.pathname + window.location.search);
     }
