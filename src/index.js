c4419fe4d7f9a4123967a334379ed46a0241bf1b
kamal <kkjoshi.it@gmail.com>
2021-07-25 08:41:24 +0530

    Revert "seo support"
    This reverts commit 33fffd75a750d3ddb4ecb038d4d7106b7a3e12aa.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { hydrate, render } from "react-dom";
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-    hydrate( <React.StrictMode>
-        <App />
-    </React.StrictMode>, rootElement);
-} else {
-    render( <React.StrictMode>
-        <App />
-    </React.StrictMode>, rootElement);
-}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
