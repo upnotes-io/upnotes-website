33fffd75a750d3ddb4ecb038d4d7106b7a3e12aa
kamal <kkjoshi.it@gmail.com>
2021-07-25 08:13:27 +0530

    seo support

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+    hydrate( <React.StrictMode>
+        <App />
+    </React.StrictMode>, rootElement);
+} else {
+    render( <React.StrictMode>
+        <App />
+    </React.StrictMode>, rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
