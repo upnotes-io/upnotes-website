fbdced959c0294f0177801d98c2c818d1a351c50
bhuwan <bhuwanchandra.it@gmail.com>
2021-12-03 09:16:27 +0530

    lint fix

--- src/components/FeatureTemplate.jsx
+++ src/components/FeatureTemplate.jsx
@@ -3,7 +3,7 @@ import ImageWithLaptopFrame from './ImageWithLaptopFrame';
 // import upnotesImg from '';
 
 const FeatureTemplate = function (props) {
-    const { title, description, backgroundColor, list, imageSrc, flexDirection = 'flex-row'  } = props;
+    const { title, description, backgroundColor, imageSrc, flexDirection = 'flex-row'  } = props;
     return <div className="flex justify-center items-center p-10" style={{ background: backgroundColor }}>
         <div className={`max-w-screen-xl pt-16 pb-16 flex flex-col  lg:${flexDirection} items-start`} >
             <div className="flex flex-col justify-center items-center py-10 lg:w-4/6 lg:px-10">
